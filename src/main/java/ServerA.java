559860e907328b7fde4969a8a291dfc89bd83147
luandersonsnts <108343352+luandersonsnts@users.noreply.github.com>
2023-06-22 11:49:44 -0300

    Update ServerA.java

--- src/main/java/ServerA.java
+++ src/main/java/ServerA.java
@@ -1,5 +1,54 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerA {
-    public double executarOperacao(double op1, char operador, double op2) {
+    public static void main(String[] args) {
+        int port = 8082; // PORTA ESCOLHIDA
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Servidor A iniciado na porta " + port);
+
+            while (true) {
+                // AGUARDA UMA CONEXÃO DO CLIENTE
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Cliente conectado: " + clientSocket.getInetAddress());
+
+                // CRIA OS STREAMS DE ENTRADA E SAÍDA DO CLIENTE
+                ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
+                ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+
+                try {
+                    // RECEBENDO A OPERAÇÃO PARA CÁLCULO
+                    double op1 = in.readDouble();
+                    char operador = in.readChar();
+                    double op2 = in.readDouble();
+
+                    // REALIZANDO A OPERAÇÃO
+                    double resultado = executarOperacao(op1, operador, op2);
+
+                    // ENVIA O RESULTADO DE VOLTA PARA O CLIENTE
+                    out.writeDouble(resultado);
+                    out.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    // FECHA AS CONEXÕES COM O CLIENTE
+                    in.close();
+                    out.close();
+                    clientSocket.close();
+                    System.out.println("Cliente desconectado: " + clientSocket.getInetAddress());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static double executarOperacao(double op1, char operador, double op2) {
+        // REALIZA A OPERAÇÃO E RETORNA O RESULTADO
         double resultado = 0.0;
         switch (operador) {
             case '+':
@@ -11,11 +60,11 @@ public class ServerA {
             case '*':
                 resultado = op1 * op2;
                 break;
-            case '/':
+            case 'd':
                 resultado = op1 / op2;
                 break;
             default:
-                System.out.println("OPERADOR INVALIDO");
+                System.out.println("OPERADOR INVÁLIDO");
         }
         return resultado;
     }
